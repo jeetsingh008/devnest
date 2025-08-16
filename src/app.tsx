a5da998e6cc580f5460ed7df070c0ba29d3efefd
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-08-16 12:12:25 +0530

    Added the navbar

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
+import NavBar from "./components/NavBar";
 import Hero from "./sections/Hero";
 import AppShowcase from "./sections/ShowcaseSection";
 
 const app = () => {
   return (
     <>
+      <NavBar />
       <Hero />
       <AppShowcase />
     </>
