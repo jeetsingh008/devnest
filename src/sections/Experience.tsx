98c4048d82ba383dd4ae7a806b7c320798d872b6
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-08-16 15:59:10 +0530

    Added the experience section

---
+++ src/sections/Experience.tsx
@@ -0,0 +1,118 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import TitleHeader from "../components/TitleHeader";
+import GlowCard from "../components/GlowCard";
+import { expCards } from "../constants";
+import type { ExpCard } from "../constants";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const Experience: React.FC = () => {
+  useGSAP(() => {
+    // Animate each timeline card on scroll
+    gsap.utils.toArray<HTMLElement>(".timeline-card").forEach((card) => {
+      gsap.from(card, {
+        xPercent: -100,
+        opacity: 0,
+        transformOrigin: "left left",
+        duration: 1,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          trigger: card,
+          start: "top 80%",
+        },
+      });
+    });
+
+    // Animate timeline height as user scrolls
+    gsap.to(".gradient-line", {
+      transformOrigin: "bottom bottom",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: ".gradient-line",
+        start: "top center",
+        end: "70% center",
+        onUpdate: (self) => {
+          gsap.to(".gradient-line", {
+            scaleY: 1 - self.progress,
+          });
+        },
+      },
+    });
+
+    // Animate expText elements on scroll
+    gsap.utils.toArray<HTMLElement>(".expText").forEach((text) => {
+      gsap.from(text, {
+        opacity: 0,
+        xPercent: 0,
+        duration: 1,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          trigger: text,
+          start: "top 60%",
+        },
+      });
+    });
+  }, []);
+
+  return (
+    <section
+      id="experience"
+      className="flex-center md:mt-40 mt-20 section-padding xl:px-0"
+    >
+      <div className="w-full h-full md:px-20 px-5">
+        <TitleHeader
+          title="Professional Work Experience"
+          sub="💼 My Career Overview"
+        />
+        <div className="mt-32 relative">
+          <div className="relative z-50 xl:space-y-32 space-y-10">
+            {expCards.map((card: ExpCard) => (
+              <div key={card.title} className="exp-card-wrapper">
+                <div className="xl:w-2/6">
+                  <GlowCard card={card} index={0}>
+                    <div>
+                      <img src={card.imgPath} alt="exp-img" />
+                    </div>
+                  </GlowCard>
+                </div>
+                <div className="xl:w-4/6">
+                  <div className="flex items-start">
+                    <div className="timeline-wrapper">
+                      {/* <div className="timeline" /> */}
+                      <div className="gradient-line w-1 h-full" />
+                    </div>
+                    <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
+                      <div className="timeline-logo">
+                        <img src={card.logoPath} alt="logo" />
+                      </div>
+                      <div>
+                        <h1 className="font-semibold text-3xl">{card.title}</h1>
+                        <p className="my-5 text-white-50">
+                          🗓️&nbsp;{card.date}
+                        </p>
+                        <p className="text-[#839CB5] italic">
+                          Responsibilities
+                        </p>
+                        <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
+                          {card.responsibilities.map((res, idx) => (
+                            <li key={idx} className="text-lg">
+                              {res}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
