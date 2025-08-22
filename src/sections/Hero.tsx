1e8f9ae4cd92401ed2cb172f8a741b2cce61b23c
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-08-22 10:19:19 +0530

    fix(ui): comment out hero experience component import
    this temporarily disables the hero experience component to resolve build issues while maintaining code availability for future use. the component was causing conflicts during the build process and needed to be isolated for further debugging.

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,5 +1,5 @@
 import Button from "../components/Button";
-import HeroExperience from "../components/HeroModels/HeroExperience";
+// import HeroExperience from "../components/HeroModels/HeroExperience";
 import { words } from "../constants";
 import "./Snow.scss";
 import { useGSAP } from "@gsap/react";
