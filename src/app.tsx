d31962350e0ab549406b5ccb6d986edce12cfb79
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-08-23 14:55:28 +0530

    refactor: remove showcase section and project images
    Remove AppShowcase component and associated project images (project1.png, project2.png, project3.png) from the application. Comment out Experience and InfiniteTestimonials components in the main app layout to streamline the user interface.

--- src/app.tsx
+++ src/app.tsx
@@ -3,20 +3,18 @@ import Contact from "./sections/Contact";
 import Experience from "./sections/Experience";
 import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
-import AppShowcase from "./sections/ShowcaseSection";
 import TechstackShow from "./sections/TechstackShow";
 import InfiniteTestimonials from "./sections/Testimonials";
 
 const app = () => {
   return (
     <>
+      {/* <LogoShowcase /> */}
+      {/* <InfiniteTestimonials /> */}
+      {/* <Experience /> */}
       <NavBar />
       <Hero />
-      <AppShowcase />
-      {/* <LogoShowcase /> */}
-      <Experience />
       <TechstackShow />
-      <InfiniteTestimonials />
       <Contact />
       <Footer />
     </>
